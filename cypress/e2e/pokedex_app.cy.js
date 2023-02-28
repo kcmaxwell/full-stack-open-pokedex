6aa550bfed496eaaa78d8b5a5b7f7b4227145e13
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-02-27 22:23:42 -0700

    Add cypress test for navigating to pokemon

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -4,4 +4,12 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate to a pokemon\'s page from the front page', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('charmander').click()
+    cy.url().should('include', '/pokemon/charmander')
+    cy.contains('blaze')
+    cy.contains('solar power')
+  })
 })
